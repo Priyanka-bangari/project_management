6fbc9f23f907210250eae3e2ed734ee4e7a81ffd
Priyanka-bangari <pbangari@argusoft.com>
2023-07-17 12:45:34 +0530

    REmove files

--- demo/src/main/java/com/example/project_manager/security/WebSecurityConfigurerAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.example.project_manager.security;
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
-public abstract class WebSecurityConfigurerAdapter {
-    protected abstract void configure(AuthenticationManagerBuilder auth) throws Exception;
-}
